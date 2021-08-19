1a028eb6ffe481656ed3892e2225126941a322a2
hmshujaatali <81732578+hmshujaatali@users.noreply.github.com>
2021-08-19 17:37:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// var d = document.getElementById("ny");
+// var p = d.childNodes[0].nodeType;
+// var contents = p.innerHTML;
+// console.log(contents)
+
+// var d = document.getElementById("humpty");
+// var pCounter = 0;
+//  for (var i = 0; i < d.childNodes.length; i++) {
+//  if (d.childNodes[i].nodeType === 1 ) {
+//  pCounter++;
+//  }
+//  if (pCounter === 1) {
+//  d.childNodes[i].innerHTML = "All his men.";
+//  break;
+//  }
+//  }
+//  var a=document.getElementById("p1").nodeValue;
+// console.log(a)
+// var target = document.getElementById("p1");
+// var attVal = target.getAttribute("class");
+
+
+// var nodeToAdd=document.createElement("p")
+// var newText=document.createTextNode("hello!")
+// nodeToAdd.appendChild(newText)
